5d2e0e6d56e22a5257145584b6bc795670760689
Jan Vansteenkiste <jan@vstone.eu>
2012-07-17 14:06:18 +0200

    Improved the config and params class a bit
    * Introduced some settings that can easily be adjusted.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,11 +1,67 @@
-# Class: logrotate::config
+# = Class: logrotate::config
 #
+# Perform initial configuration.
 #
-class logrotate::config {
-  file { $logrotate::params::confdir:
-    owner   => root,
-    group   => root,
-    ensure  => directory,
-    require => Class['logrotate::install'],
+# == Parameters:
+#
+# $rotation_interval::  Global rotation interval (daily, weekly, ...)
+#
+# $retention_period::   Retention period.
+#
+# === Advanced configuration:
+#
+# Normally you will not have to touch these. In case you have a funky setup
+# (custom package?) you can override these additional parameters:
+#
+# $confdir::    Directory where additional logrotation configuration is placed.
+#
+# $conffile::   Main logrotate configuration file.
+#
+# == Actions:
+#
+# This will make sure that the configuration directory and file exist and
+# initialize the configuration file with some settings.
+#
+# == Requires:
+#
+# A logrotate package should be installed. If the package has another name,
+# make sure to alias it:
+#
+#     package {'custom-logrotate':
+#       ensure => 'installed',
+#       alias => 'logrotate',
+#     }
+#
+# == Todo:
+#
+# TODO: Update documentation
+#
+class logrotate::config (
+  $confdir  = $::logrotate::params::conf_dir,
+  $conffile = $::logrotate::params::conf_file,
+  $rotation_interval = $::logrotate::params::rotation_interval,
+  $retention_period =  $::logrotate::params::retention_period
+) inherits logrotate::params {
+
+  File {
+    require => Package['logrotate'],
   }
+
+  file { 'logrotate.d':
+    ensure => 'directory',
+    path   => $confdir,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file {'logrotate.conf':
+    ensure  => 'present',
+    path    => $conffile,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('logrotate/logrotate.conf.erb'),
+  }
+
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,63 @@
 # Class logrotate::params
 #
 #
-class logrotate::params {
-  case $::operatingsystem {
-    'debian', 'ubuntu', 'RedHat', 'CentOS', 'Fedora': {
-      $pkg     = 'logrotate'
-      $confdir = '/etc/logrotate.d'
-    }
+# == Parameters:
+#
+# $package::            Override the package name to be installed.
+#                       Defaults to distro-specific or 'logrotate'.
+#
+# $confdir::            Override the logrotation directory.
+#                       Defaults to distro-specific or '/etc/logrotate.d'.
+#
+# $conffile::           Override the default main configuration file.
+#                       Defaults to distro-specific or '/etc/logrotate.conf'.
+#
+# $rotation_interval::  The default rotation interval to use. This is
+#                       configured in the main configuration file.
+#                       Defaults to 'daily'.
+#
+# $retention_period::   The default retention period to use. This is configured
+#                       in the main configuration file.
+#                       Defaults to 90.
+#
+# == Actions:
+#
+# == Requires:
+#
+# == Sample Usage:
+#
+class logrotate::params (
+  $package = undef,
+  $confdir = undef,
+  $conffile = undef,
+  $rotation_interval = 'daily',
+  $retention_period = 90
+) {
 
-    default: {
-      fail 'Operating system not supported yet.'
-    }
+  $pkg = $package ? {
+    undef   => $::operatingsystem ? {
+      /(?i:redhat|centos|fedora)/ => 'logrotate',
+      /(?i:debian|ubuntu)/        => 'logrotate',
+      default                     => 'logrotate',
+    },
+    default => $package,
   }
+  $conf_dir = $confdir ? {
+    undef   => $::operatingsystem ? {
+      /(?i:redhat|centos|fedora)/ => '/etc/logrotate.d',
+      /(?i:debian|ubuntu)/        => '/etc/logrotate.d',
+      default                     => '/etc/logrotate.d',
+    },
+    default => $confdir,
+  }
+
+  $conf_file = $conffile ? {
+    undef   => $::operatingsystem ? {
+      /(?i:redhat|centos|fedora)/ => '/etc/logrotate.conf',
+      /(?i:debian|ubuntu)/        => '/etc/logrotate.conf',
+      default                     => '/etc/logrotate.conf',
+    },
+    default => $conffile,
+  }
+
 }
