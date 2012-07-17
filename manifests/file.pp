740295f0f9a032e8f17f4ab9b9279c5e12119cda
Jan Vansteenkiste <jan@vstone.eu>
2012-07-17 14:02:01 +0200

    Added documentation

--- manifests/file.pp
+++ manifests/file.pp
@@ -1,3 +1,29 @@
+# = Definition: logrotate::file
+#
+# == Parameters:
+#
+# $log::        Array or string which contains paths/patterns to match.
+#               This is a required parameter and has no default value.
+#
+# $ensure::     See the file puppet type for more information.
+#
+# $path::       Override the location of the configuration file.
+#               This is by default the conf_dir.
+#               conf_dir can be configured globally in logrotate::params.
+#
+# $options::    Array or string with options for this logrotation definition.
+#               Defaults to [ 'weekly', 'compress', 'rotate 7', 'missingok' ].
+#
+# $prerotate::  Command(s) to execute before rotation.
+#               Defaults to empty (undefined).
+#
+# $postrotate:: Command(s) to execute after rotation.
+#               Defaults to empty (undefined).
+#
+# == Todo:
+#
+# TODO: Update documentation (Add an example).
+#
 define logrotate::file (
   $log,
   $ensure     = 'present',
